aa52d23dab78b2dab505328abfc0815495541bd1
psurkov <petyo333@gmail.com>
2022-04-08 19:14:33 +0300

    dummy formatter

--- psiminer-core/src/main/kotlin/psi/nodeProperties/Token.kt
+++ psiminer-core/src/main/kotlin/psi/nodeProperties/Token.kt
@@ -5,19 +5,13 @@ import astminer.common.splitToSubtokens
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 
-const val EMPTY_TOKEN = "<E>"
+const val EMPTY_TOKEN_OLD = "<EMPTY>"
 
 val PsiElement.token: String?
     get() = when {
-        PsiTreeUtil.firstChild(this) != this -> EMPTY_TOKEN // if not leaf then there is no token
+        PsiTreeUtil.firstChild(this) != this -> EMPTY_TOKEN_OLD // if not leaf then there is no token
         technicalToken != null -> technicalToken
-        else -> normalizedToken
-    }
-
-val PsiElement.normalizedToken: String
-    get() = splitToSubtokens(text).let {
-        if (it.isEmpty()) EMPTY_TOKEN
-        else it.joinToString("|")
+        else -> text
     }
 
 var PsiElement.technicalToken: String?
